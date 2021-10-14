e86ba967d70d32b7afcca5d8d1e37afd4e890c14
Pedro Goulart <pedrogoulart555@gmail.com>
2021-10-14 15:04:24 -0300

    pesquisa restaurada para ultimo estado funcional

--- api/api.php
+++ api/api.php
@@ -1,10 +1,8 @@
 <?php
-require_once('../dbhost.php');
+require_once('../environment.php');
 require_once('../Source/Database.php');
-$database = new Database($host, $dbusr, $dbpwd);
+$database = new Database($_ENV['DATABASE_NAME'], $_ENV['DATABASE_HOST'], $_ENV['DATABASE_USR'], $_ENV['DATABASE_PWD']);
 require_once("./api.callbacks.php");
 
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
-
-//echo routeRequest($_GET['page']);
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
